c945d4ab92d8b2f59029ed866048ccc026f9e15d
Aaron Liblong <liblonga@physics.utoronto.ca>
2016-07-15 06:33:33 +0200

    add default-val accessors

--- include/json_reader.h
+++ include/json_reader.h
@@ -21,12 +21,32 @@ class JSONReader {
   }
 
   template<typename T>
-  std::vector<T> get_vector(const std::string& key) const {
+  T get(const std::string& key, T default_val) const {
+    return pt.get<T>(key, default_val);
+  }
+
+  template<typename T>
+  auto get_vector(const std::string& key) const {
     std::vector<T> vec;
     BOOST_FOREACH(const ptree::value_type &child, pt.get_child(key)) {
       vec.push_back(child.second.get_value<T>());
     }
     return vec;
+  }
+
+  template<typename T>
+  auto get_vector(const std::string& key, std::vector<T>&& default_val) const {
+    std::vector<T> vec;
+    auto node = pt.get_child_optional(key);
+    if (!node) {
+      return default_val;
+    }
+    else {
+      BOOST_FOREACH(const ptree::value_type &child, *node) {
+        vec.push_back(child.second.get_value<T>());
+      }
+    return vec;
+    }
   }
 
   template<typename T>
@@ -39,12 +59,44 @@ class JSONReader {
   }
 
   template<typename T>
+  auto get_map(const std::string& key, boost::container::flat_map<std::string,T>&& default_val) const {
+    boost::container::flat_map<std::string, T> result;
+    auto node = pt.get_child_optional(key);
+    if (!node) {
+      return default_val;
+    }
+    else {
+      BOOST_FOREACH(const ptree::value_type &child, *node) {
+       result.insert(std::make_pair(child.first, child.second.get_value<T>()));
+      }
+      return result;
+    }
+  }
+
+  template<typename T>
   auto get_map_of_vectors(const std::string& key) const {
     boost::container::flat_map<std::string, std::vector<T>> result;
     for (const auto& child: pt.get_child(key)) {
       auto key_to_insert = child.first;
       auto val_to_insert = get_vector<T>(key+key_to_insert);
       result.insert(std::make_pair(key_to_insert, val_to_insert));
+    }
+    return result;
+  }
+
+  template<typename T>
+  auto get_map_of_vectors(const std::string& key, boost::container::flat_map<std::string, std::vector<T>>&& default_val) const {
+    boost::container::flat_map<std::string, std::vector<T>> result;
+    auto node = pt.get_child_optional(key);
+    if (!node) {
+      return default_val;
+    }
+    else {
+      for (const auto& child: *node) {
+        auto key_to_insert = child.first;
+        auto val_to_insert = get_vector<T>(key+key_to_insert);
+        result.insert(std::make_pair(key_to_insert, val_to_insert));
+      }
     }
     return result;
   }
